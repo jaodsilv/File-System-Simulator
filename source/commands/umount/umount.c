88b39485dee4f06fd1ea8f3a9d905ac0ac500b8c
Fichberg <rfichberg@gmail.com>
2015-11-14 01:33:52 -0200

    umount TODO

--- source/commands/umount/umount.c
+++ source/commands/umount/umount.c
@@ -5,7 +5,7 @@ int cmd_umount(char *cmd, char *file_system, Directory *root_dir, bool mounted)
   if(strncmp(cmd, "umount", 6) == 0) {
     /*Unmount file system from this file_system*/
     if(mounted) {
-
+      /*TODO: free every node except the root*/
       printf("File system sucessfully unmounted!\n");
     }
     else
